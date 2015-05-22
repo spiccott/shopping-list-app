d471c87ff4b0a702303fbcfab38245bb10a8228c
Stephen Piccott <stevepiccott@gmail.com>
2015-05-22 00:05:47 -0230

    Adds checkboxes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,31 @@
 $(document).ready(function() {
+	// var incompleteItems = [];
+
+
 	$(document).keyup(function(e) {
 		var enterKey = e.keyCode || e.which;
 		if(enterKey == 13) {
 			var listItem = $("#item-input").val();
-			$("#incomplete-items>ul").prepend( "<li><input type='checkbox'/>" + listItem + "</li>" );
-			$("#item-input").val("");
+			if(listItem.length > 0)
+				$("#incomplete-items>ul").prepend( "<li><div><input type='checkbox' class='checkbox' id='check'/></div><div>" + listItem + "</div></li>" );
+				$("#item-input").val("");
 		}
 	});
+
+	// $("input:checkbox:checked").
+
+	// $("input:checkbox").on("click", function() {
+	// 	if (this.checked == True) {
+	// 		this.
+	// 	}
+	// })
+	$("input[type=checkbox]").on("click", function() {
+		verifyChecked();
+	});
 });
+
+function verifyChecked() {
+	if (document.getElementById("check").checked) {
+		this.closest("li").insertAfter("#complete-items>ul");
+	}
+}
