d31d96ca32e76cd12dba55c51952987ee697bba2
Stephen Piccott <stevepiccott@gmail.com>
2015-06-16 12:46:22 -0230

    Adds jquery on click event listeners for check/uncheck and remove, fixes stylesheet redundency, cleans up index.html test code

--- js/app.js
+++ js/app.js
@@ -1,31 +1,33 @@
 $(document).ready(function() {
-	// var incompleteItems = [];
-
-
 	$(document).keyup(function(e) {
 		var enterKey = e.keyCode || e.which;
 		if(enterKey == 13) {
 			var listItem = $("#item-input").val();
 			if(listItem.length > 0)
-				$("#incomplete-items>ul").prepend( "<li><div><input type='checkbox' class='checkbox' id='check'/></div><div class='list-item'>" + listItem + "</div></li>" );
+				$("#item-list>ul").prepend( "<li class='items'><span class='item-check incomplete'></span><span class='item-text'>" + listItem + "</span><span class='item-remove'><img src='images/remove.png' alt='remove' width='20px'></span></li>" );
+				$("li:first-child").css('opacity', "0")
+				.animate(
+				    { opacity: "1" },
+				    { queue: true, duration: 'slow' }
+				);
 				$("#item-input").val("");
 		}
 	});
-
-	// $("input:checkbox:checked").
-
-	// $("input:checkbox").on("click", function() {
-	// 	if (this.checked == True) {
-	// 		this.
-	// 	}
-	// })
-	$("input[type=checkbox]").on("click", function() {
-		verifyChecked();
+	$("#item-list>ul").on('click', '.item-check', function(event) {
+		if($(this).hasClass( "incomplete" )) {
+			console.log('Clicked');
+			$(this).removeClass("incomplete");
+			$(this).addClass("complete");
+			$(this).parent().css({'background-color': 'rgba(255,255,255,0)', 'text-decoration': 'line-through', 'color': '#999999'})
+		}
+		else {
+			console.log('Clicked');
+			$(this).removeClass("complete");
+			$(this).addClass("incomplete");
+			$(this).parent().css({'background-color': 'rgba(255,255,255,0.8)', 'text-decoration': 'none', 'color': '#131313'})
+		}
+	});
+	$("#item-list>ul").on('click', '.item-remove', function(event) {
+		$(this).parent().remove();
 	});
 });
-
-function verifyChecked() {
-	if (document.getElementById("check").checked) {
-		$("input").closest("li").insertAfter("#complete-items>ul");
-	}
-}
